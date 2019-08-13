11766e3c6c6da7f5536999608499c59fc8c22c1f
Rick Fletcher <fletch@pobox.com>
2019-08-13 14:27:00 -0400

    Formatting, module upgrades

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import pino from "pino";
 import Snips from "./lib/snips.js";
 
+// default config
 var config = {
   logger: pino({ level: "debug" }),
   mqtt: {
@@ -14,14 +15,15 @@ var config = {
     wakeword: "default",
   },
 };
-
 config.mqtt.port = config.mqtt.port || (config.mqtt.ssl ? 8883 : 1883);
 
+// initailize
 const snips = new Snips(config);
-
-snips.push("fletch@pobox.com", "is it hot?");
-snips.push("fletch@pobox.com", "outside");
 
 snips.on("text", function(who, what) {
   config.logger.info(`Send to ${who}: ${what}`);
 });
+
+// send a test message
+snips.push("fletch@pobox.com", "is it hot outside?");
+// snips.push("fletch@pobox.com", "outside");
